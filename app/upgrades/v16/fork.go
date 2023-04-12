ea337fcb7a21ff17d36ba10d3d00a3810ba711c5
arlai-mk <arlai@moonkitt.com>
2023-04-12 09:09:38 +0000

    Send Huahua gauges funds to Huahua Team Wallet It will also automatically archive the gauges after the next Epoch distribution

--- app/upgrades/v16/fork.go
+++ app/upgrades/v16/fork.go
@@ -3,6 +3,8 @@ package v16
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
+	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
+
 	incentiveskeeper "github.com/osmosis-labs/osmosis/v15/x/incentives/keeper"
 
 	"github.com/osmosis-labs/osmosis/v15/app/keepers"
@@ -16,32 +18,54 @@ func RunForkLogic(ctx sdk.Context, appKeepers *keepers.AppKeepers) {
 	}
 	ctx.Logger().Info("Applying Osmosis v16 upgrade.")
 	ctx.Logger().Info("Fixing the Huahua gauges.")
-	FixHuahuaGauges(ctx, appKeepers.IncentivesKeeper)
+	FixHuahuaGauges(ctx, appKeepers.IncentivesKeeper, appKeepers.BankKeeper)
 }
 
 // Some gauges were incorrectly created for pools 605 (HUAHUA / OSMO) and 606 (HUAHUA / ATOM)
 // and this error prevented them from distributing 6 billion HUAHUA tokens as incentives.
 // This fix is here to release these incentives to their respective pools correctly.
-func FixHuahuaGauges(ctx sdk.Context, incentiveskeeper *incentiveskeeper.Keeper) {
+func FixHuahuaGauges(ctx sdk.Context, incentiveskeeper *incentiveskeeper.Keeper, bank bankkeeper.Keeper) {
 	// Huahua incorrect gauges are IDs 1954 to 1959
 	gaugeIDs := []uint64{1954, 1955, 1956, 1957, 1958, 1959}
+
+	// Huahua Team wallet is osmo14fketv99hlrlk80mkggw643spsj3yyf7t2pjhr
+	const huahuaTeamWalletAddrString = "osmo14fketv99hlrlk80mkggw643spsj3yyf7t2pjhr"
+
+	huahuaTeamWalletAddr, err := sdk.AccAddressFromBech32(huahuaTeamWalletAddrString)
+	if err != nil {
+		ctx.Logger().Error("v16 upgrade: Could not parse Huahua team wallet")
+		panic(err)
+	}
 
 	gauges, err := incentiveskeeper.GetGaugeFromIDs(ctx, gaugeIDs)
 
 	if err != nil {
 		ctx.Logger().Error("v16 upgrade: Could not find the Huahua gauges for fixing")
-		return
+		panic(err)
 	}
 
-	// Fix the QueryCondition Denom
+	allCoins := sdk.Coins{}
+
 	for _, gauge := range gauges {
-		if gauge.DistributeTo.Denom == "GAMM605" {
-			gauge.DistributeTo.Denom = "pool/gamm/605"
-		} else if gauge.DistributeTo.Denom == "GAMM606" {
-			gauge.DistributeTo.Denom = "pool/gamm/606"
-		}
+		// retrieve the exact amount of coins from the gauge
+		allCoins = append(allCoins, gauge.Coins...)
+
+		// reset the gauge to no coin
+		gauge.Coins = sdk.Coins{}
+		// mark the gauge as filled over the required num of epochs
+		gauge.FilledEpochs = gauge.NumEpochsPaidOver
 	}
 
 	// Overwrite the gauges in the store
-	incentiveskeeper.OverwriteGaugeV16MigrationUnsafe(ctx, gauges)
+	err = incentiveskeeper.OverwriteGaugeV16MigrationUnsafe(ctx, gauges)
+	if err != nil {
+		ctx.Logger().Error("v16 upgrade: Could not overwrite Huahua gauges")
+		panic(err)
+	}
+
+	err = bank.SendCoinsFromModuleToAccount(ctx, "incentives", huahuaTeamWalletAddr, allCoins)
+	if err != nil {
+		ctx.Logger().Error("v16 upgrade: Could not send coins to Huahua team wallet")
+		panic(err)
+	}
 }
